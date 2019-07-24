c5cdb41f35490e71fa4af46029f8d65b9256c951
NikosSiak <nikos.siakas@gmail.com>
2019-07-24 16:03:32 +0300

    added srand

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <time.h>
 #include "Chip8.hpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    srand(time(nullptr));
     Chip8 chip;
     chip.loadGame(argv[1]);
     return 0;
