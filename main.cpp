cc601622adb9c8c95840afd69ef5a223c4314530
NikosSiak <nikos.siakas@gmail.com>
2019-07-24 16:07:31 +0300

    added srand

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    srand(time(nullptr));
+    srand(static_cast<unsigned int>(time(nullptr)));
     Chip8 chip;
     chip.loadGame(argv[1]);
     return 0;
